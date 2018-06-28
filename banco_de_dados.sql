66592fba0df4bf0df67101d7aae7d55cfbac43f8
SFalcon12 <israelvipe@gmail.com>
2018-06-27 22:12:42 -0300

    123

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.1
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2018 at 06:35 AM
--- Server version: 10.1.33-MariaDB
--- PHP Version: 7.2.6
+-- Generation Time: 28-Jun-2018 às 03:11
+-- Versão do servidor: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,7 +27,7 @@ USE `agenda`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_cidades`
+-- Estrutura da tabela `tbl_cidades`
 --
 
 CREATE TABLE `tbl_cidades` (
@@ -39,7 +39,7 @@ CREATE TABLE `tbl_cidades` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_contatos`
+-- Estrutura da tabela `tbl_contatos`
 --
 
 CREATE TABLE `tbl_contatos` (
@@ -56,7 +56,7 @@ CREATE TABLE `tbl_contatos` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_telefones`
+-- Estrutura da tabela `tbl_telefones`
 --
 
 CREATE TABLE `tbl_telefones` (
@@ -69,12 +69,13 @@ CREATE TABLE `tbl_telefones` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_usuarios`
+-- Estrutura da tabela `tbl_usuarios`
 --
 
 CREATE TABLE `tbl_usuarios` (
   `id` int(11) NOT NULL,
   `nome_usuario` varchar(30) NOT NULL,
+  `email` varchar(40) NOT NULL,
   `senha` char(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -116,38 +117,34 @@ ALTER TABLE `tbl_usuarios`
 -- AUTO_INCREMENT for table `tbl_cidades`
 --
 ALTER TABLE `tbl_cidades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `tbl_contatos`
 --
 ALTER TABLE `tbl_contatos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT for table `tbl_telefones`
 --
 ALTER TABLE `tbl_telefones`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
 --
 -- AUTO_INCREMENT for table `tbl_usuarios`
 --
 ALTER TABLE `tbl_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `tbl_contatos`
+-- Limitadores para a tabela `tbl_contatos`
 --
 ALTER TABLE `tbl_contatos`
   ADD CONSTRAINT `fk_tbl_contatos_tbl_cidades` FOREIGN KEY (`cidade_id`) REFERENCES `tbl_cidades` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `tbl_telefones`
+-- Limitadores para a tabela `tbl_telefones`
 --
 ALTER TABLE `tbl_telefones`
   ADD CONSTRAINT `fk_tbl_telefones_tbl_contatos1` FOREIGN KEY (`contato_id`) REFERENCES `tbl_contatos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
