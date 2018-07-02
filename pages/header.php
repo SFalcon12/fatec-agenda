a5075208122f05815661b0016af7b3feb753daa3
SFalcon12 <israelvipe@gmail.com>
2018-07-02 20:07:43 -0300

    titulo

--- pages/header.php
+++ pages/header.php
@@ -3,6 +3,8 @@
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
+<title>Agenda</title>
+
 <link href="../img/favicon.ico" rel="icon" type="image/x-icon" />
 <link rel="stylesheet" href="../css/vendor/bootstrap/bootstrap.css"/>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,200,400,600"/>
